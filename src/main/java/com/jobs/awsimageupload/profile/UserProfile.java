fad5dabfeaf5b5fa73d4e5ca73de1b77809593e6
Park Joyeong <drhamst@gmail.com>
2021-04-19 22:39:42 +0900

    Commit...

---
+++ src/main/java/com/jobs/awsimageupload/profile/UserProfile.java
@@ -0,0 +1,10 @@
+package com.jobs.awsimageupload.profile;
+
+import java.util.UUID;
+
+public class UserProfile {
+
+    private UUID userProfileId;
+    private String username;
+    private String userProfileImageLink; // S3 link
+}
