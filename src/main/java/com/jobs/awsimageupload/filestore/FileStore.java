c86aaaca1549ec49606784215e7e01e5f42bf5f2
Park Joyeong <drhamst@gmail.com>
2021-04-19 17:24:52 +0900

    I have really no idea

--- src/main/java/com/jobs/awsimageupload/filestore/FileStore.java
+++ src/main/java/com/jobs/awsimageupload/filestore/FileStore.java
@@ -1,6 +1,8 @@
 package com.jobs.awsimageupload.filestore;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +24,19 @@ public class FileStore {
                      String fileName,
                      Optional<Map<String, String>> optionalMetadata,
                      InputStream inputStream) {
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        optionalMetadata.ifPresent(map -> {
+            if (!map.isEmpty()) {
+                map.forEach(metadata::addUserMetadata);
+            }
+        });
+
+        try {
+            s3.putObject(path, fileName, inputStream, metadata);
+        } catch (AmazonServiceException e) {
+            throw new IllegalStateException("Failed to store file to s3", e);
+        }
 
     }
 }
