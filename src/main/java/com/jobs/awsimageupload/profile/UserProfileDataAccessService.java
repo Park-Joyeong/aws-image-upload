9baabd4b5f3501a6188cdeb83ba1946cc733b243
Park Joyeong <drhamst@gmail.com>
2021-04-21 23:38:16 +0900

    I'm adding run config=> Environment variable: AWS_REGION=ap-northeast-2

--- src/main/java/com/jobs/awsimageupload/profile/UserProfileDataAccessService.java
+++ src/main/java/com/jobs/awsimageupload/profile/UserProfileDataAccessService.java
@@ -1,9 +1,23 @@
 package com.jobs.awsimageupload.profile;
 
+import com.jobs.awsimageupload.datastore.FakeUserProfileDataStore;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class UserProfileDataAccessService {
 
+    private final FakeUserProfileDataStore fakeUserProfileDataStore;
+
+    @Autowired
+    public UserProfileDataAccessService(FakeUserProfileDataStore fakeUserProfileDataStore) {
+        this.fakeUserProfileDataStore = fakeUserProfileDataStore;
+    }
+
+    List<UserProfile> getUserProfiles() {
+        return fakeUserProfileDataStore.getUserProfiles();
+    }
 
 }
